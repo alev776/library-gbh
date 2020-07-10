54de8be5ff673e9f5c434cd0a5dd92648e2ca58a
Alex <alrv0157@gmail.com>
2020-07-10 17:31:50 -0400

    Updating app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(books);
 app.use(pages);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
